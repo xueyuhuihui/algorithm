49392fdc7dcf659049b445c2fd639cfe3b613ec1
wuyihui <351893759@qq.com>
2019-07-27 17:50:35 +0800

    update 1_two_sum

---
+++ Codes/xueyuhuihui/1_two_sum/Solution.java
@@ -0,0 +1,40 @@
+
+class Solution {
+
+    /**
+     *
+     * 执行结果：     通过
+     * 显示详情
+     * 执行用时 :     10 ms
+     * 内存消耗 :     39.4 MB
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        int temp = 0;
+        int length = nums.length;
+        Set<Integer> tempSet = new HashSet<>();
+        for (int i = 0; i < length; i++) {
+            temp = target - nums[i];
+            tempSet.add(temp);
+        }
+        for (int i = 0; i < length; i++) {
+            boolean contains = tempSet.contains(nums[i]);
+            if (contains) {
+                for (int j = 0; j < length; j++) {
+                    //System.out.println("no_found"+nums[i] + "-" + nums[j]);
+                    if (target == (nums[i] + nums[j]) && i != j) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+        }
+
+        System.out.print("no_found");
+        return null;
+    }
+
+
+}
